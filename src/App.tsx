d713fe59d1e1565da2752afa64ee9e2ac03ebb3b
张一 <zy01066705@alibaba-inc.com>
2024-02-06 09:51:35 +0800

    chore: 1

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Tabs, type TabsProps } from 'antd';
+import Login from './Login';
+import Register from './Register';
+
+const items: TabsProps['items'] = [
+  {
+    key: '1',
+    label: '登录',
+    children: <Login />,
+  },
+  {
+    key: '2',
+    label: '注册',
+    children: <Register />,
+  },
+];
+
+const App = () => {
+  return <Tabs defaultActiveKey="1" items={items} />;
+};
+
+export default App;
